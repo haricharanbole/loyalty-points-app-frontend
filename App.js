1b11c0bdb0de93f57ec45c0c15510e03c4762b99
/ <hustle@haricharans-MacBook-Air.local>
2020-06-08 21:52:50 +0530

    Refactor App.js

--- App.js
+++ App.js
@@ -3,33 +3,22 @@ import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {
-  StatusBar,
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
-  Platform,
-} from 'react-native';
-
-const Stack = createStackNavigator();
-
 import SelectBrands from './src/components/screens/SelectBrands';
 import HomeScreen from './src/components/screens/HomeScreen';
 import SelectOutlet from './src/components/screens/SelectOutlet';
 import ViewOutlet from './src/components/screens/ViewOutlet';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-console.log(windowWidth, windowHeight, Platform.OS);
+const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
     return (
       <>
         <NavigationContainer>
-          <Stack.Navigator>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}>
             <Stack.Screen name="Brand Selection" component={SelectBrands} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Outlet Selection" component={SelectOutlet} />
